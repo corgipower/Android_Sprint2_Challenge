fd2868d0fa130f2910f9a58f85b378a22fc0916b
corgipower <greensborodogtraining@gmail.com>
2019-08-20 16:11:30 -0400

    About half finished, I hope.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/util/GroceryListAdapter.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/util/GroceryListAdapter.kt
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -26,11 +28,17 @@ class GroceryListAdapter(val groceryList: MutableList<Groceries>) : RecyclerView
             grocery.isSelected = !grocery.isSelected
             notifyItemChanged(position)
         }
+        setEnterAnimation(holder.groceryListParent, position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.grocery_list_item, parent, false) as View)
+    }
+
+    fun setEnterAnimation(viewToAnimate: View, position: Int) {
+        val animation: Animation = AnimationUtils.loadAnimation(viewToAnimate.context, android.R.anim.slide_in_left)
+        viewToAnimate.startAnimation(animation)
     }
 
 
